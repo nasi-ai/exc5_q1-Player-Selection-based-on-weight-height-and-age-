31f242a90fec00c9955352a4e85efafef286c342
nasi-ai <uni.akbari.n@gmail.com>
2023-07-10 00:27:12 +0330

    age validation done in getter method

--- main.py
+++ main.py
@@ -1,16 +1,73 @@
-# This is a sample Python script.
-
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+import random
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+class Participant:
+    def __init__(self):
+        self._participant_code = self._make_participant_code()
+        self._name = None
+        self._lastname = None
+        self._age = -1
+        self._height = -1
+        self._weight = -1
+# def __init__(self, name, lastname, age, height, weight):
+#         self._participant_code = self._make_participant_code()
+#         self._name = name
+#         self._lastname = lastname
+#         self._age = age
+#         self._height = height
+#         self._weight = weight
+
+    @staticmethod
+    def _make_participant_code():
+        rnd = random.randint(1000, 2000)
+        return rnd
+    
+    @property
+    def name(self):
+        return self.name
+    
+    @name.setter
+    def name(self, value):
+        self._name = value
+    
+    @property
+    def lastname(self):
+        return self.lastname
+    
+    @lastname.setter
+    def lastname(self, value):
+        self._lastname = value
+
+    @property
+    def age(self):
+        return self._age
+
+    @age.setter
+    def age(self, age):
+        if not isinstance(age, int):
+            raise TypeError('Age must be an integer.')
+        if not (15 <= age <= 35):
+            raise ValueError('Age must be between 15 and 35.')
+        self._age = age
+
+    @property
+    def height(self):
+        return 
+    
+    @height.setter
+    def height(self, value):
+        pass
+
+    @property
+    def weight(self):
+        return 
+    
+    @weight.setter
+    def weight(self, value):
+        pass
 
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+
+o = Participant()
+o.age = 35
